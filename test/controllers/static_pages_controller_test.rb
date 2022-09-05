b8c97d485f961da4493257527ce9ca07f3141ba4
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2022-09-05 13:51:48 +0900

    静的ページの作成

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
-
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -14,5 +21,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
+    puts "#{@base_title}"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
